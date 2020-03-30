2600be7e404207279facd965aecd90bd780e24bd
davidnewtral <62879108+davidnewtral@users.noreply.github.com>
2020-03-30 11:25:37 +0200

    Create chapters.js

---
+++ chapters.js
@@ -0,0 +1,120 @@
+const chapters = [
+  {
+    id: "Intro",
+    title: "",
+    image: "",
+    description: "",
+    country: "",
+    location: {
+      center: [13.41443, 45.95619],
+      zoom: 4.38,
+      pitch: 0.0,
+      bearing: 0.0,
+      speed: 1
+    }
+  },
+  {
+    id: "madrid-lights",
+    title: "Madrid",
+    image: "",
+    country: "Spain",
+    description:
+      'En el último día ha habido 🇪🇸 {"fallecidos": 1000} y  {"recuperados": 458} adicionales respecto al día anterior.',
+    marker: [-3.69157, 40.42324],
+    location: {
+      center: [0.74772, 40.26061],
+      zoom: 5.2,
+      pitch: 0.0,
+      bearing: 2.05,
+      speed: 0.6
+    }
+  },
+  {
+    id: "national",
+    title: "Madrid",
+    description:
+      "El ayuntamiento de Madrid ya ha dispuesto IFEMA como hospital de campaña, y una pista de hielo como morgue temporal.",
+
+    country: "Spain",
+    marker: [-3.69157, 40.42324],
+    location: {
+      center: [0.74772, 40.26061],
+      zoom: 5.2,
+      pitch: 0.0,
+      bearing: 2.05,
+      speed: 0.6
+    }
+  },
+  {
+    id: "germany",
+    title: "Alemania",
+    image: "",
+    description:
+      'En alemania, la forma de contabilizar los casos y fallecimientos por COVID-19 ha provocado que su curva de crecimiento sea más baja de lo esperado. A día 25 de Marzo tiene {"casos-activos": 33570}',
+    country: "Germany",
+    location: {
+      center: [9.8277, 51.00773],
+      zoom: 5.77,
+      pitch: 55.5,
+      bearing: -59.2,
+      speed: 0.6
+    }
+  },
+  {
+    id: "italy",
+    title: "Italia",
+    image: "",
+    description:
+      'Italia, el gran epicentro del COVID-19 en Europa, ha llegado a contabilizar {"fallecidos": 793} en un mismo día.',
+    location: {
+      center: [14.59267, 40.63972],
+      zoom: 5.81,
+      pitch: 48.0,
+      bearing: 44.0,
+      speed: 0.6
+    }
+  },
+  {
+    id: "wuhan",
+    title: "Wuhan",
+    image: "",
+    description:
+      'Se cree que en Wuhan se originó el virus del COVID-19. En la provincia de Hubei se han registrado {"casos": 67800} y {"fallecidos": 3133}',
+    marker: [114.26667, 30.58333],
+    location: {
+      center: [114.25458, 30.51456],
+      zoom: 4.33,
+      pitch: 43,
+      bearing: -12,
+      speed: 1
+    }
+  },
+  {
+    id: "us",
+    title: "Estados Unidos",
+    image: "",
+    description:
+      'A día de hoy, el país que más está creciendo es Estados Unidos, con {"casos-activos": 13355} más sólo el día 24 de Marzo.',
+    location: {
+      center: [-99.05527, 41.47444],
+      zoom: 3.88,
+      pitch: 52.0,
+      bearing: 8.8,
+      speed: 1
+    }
+  },
+  {
+    id: "nyc",
+    title: "Nueva York",
+    image: "",
+    description:
+      'En el estado de Nueva York ya se han reportado más de {"casos": 30000}, con alrededor de un 12% de ellos hospitalizados, y un 8% en cuidados intensivos (UCI)',
+    location: {
+      center: [-73.73714, 42.51073],
+      zoom: 6.89,
+      pitch: 40.5,
+      bearing: -39.29,
+      speed: 1
+    }
+  }
+];
